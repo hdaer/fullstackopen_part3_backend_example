279e9bafbc93bf2c92887995bda26e0e8f699eaa
hdaer <hardar333@hotmail.com>
2025-04-04 16:31:50 +0200

    uibuild

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(requestLogger)
 
 
@@ -27,11 +27,15 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+        if (note) {
+            response.json(note)
+        }
+        else {
+            response.status(404).send('a note with this id could not be found').end()
+        }
+    }).catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -52,25 +56,31 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id
-    const body = request.body
+    const { content, important } = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+    Note.findById(request.params.id).then(
 
-    Note.findByIdAndUpdate(
-        id,
-        { content: body.content, important: body.important },
-        { new: true }
-    ).then(result => {
-        if (result) {
-            response.json(result)
-        } else {
-            response.status(404).json({ error: 'note not found' })
-        }
-    })
-        .catch(error => next(error))
+    )
+
+    // const id = request.params.id
+    // const body = request.body
+
+    // if (!body.content) {
+    //     return response.status(400).json({ error: 'content missing' })
+    // }
+
+    // Note.findByIdAndUpdate(
+    //     id,
+    //     { content: body.content, important: body.important },
+    //     { new: true }
+    // ).then(result => {
+    //     if (result) {
+    //         response.json(result)
+    //     } else {
+    //         response.status(404).json({ error: 'note not found' })
+    //     }
+    // })
+    //     .catch(error => next(error))
 })
 
 
@@ -84,7 +94,6 @@ app.delete('/api/notes/:id', (request, response, next) => {
             }
         })
         .catch(error => next(error))
-
 })
 
 const unknownEndpoint = (request, response) => {
@@ -93,6 +102,16 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
